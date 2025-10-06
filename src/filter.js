1692318f066fa83bb82e0886fcb63c299d27421f
tuhinx <20234208032@eee.bubt.edu.bd>
2025-10-06 23:18:07 +0600

    Update

--- src/filter.js
+++ src/filter.js
@@ -1,10 +1,11 @@
 // Export an array of allowed IDs. You can also `export default [...]`.
 export const ids = [
-	 'btsport1',
-	 'tsportshd',
-	 'dubaisports1',
-	 'asports',
-	 'starsportselect1',
+	'asports',
+	'starsportselect1',
+	'btsport1',
+	'tsportshd',
+	'dubaisports1',
+
 ];
 
 export default ids;
